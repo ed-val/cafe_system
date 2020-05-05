f76097eb6011e447363003543f9249fe82d6d9bd
ed-val2 <e.valbuena.d@gmail.com>
2020-05-05 00:28:39 -0500

    created a first working fifo method

--- src/methods/fifo.js
+++ src/methods/fifo.js
@@ -1,30 +1,106 @@
-import fs from 'fs';
+import Menu from './menu';
 
-export function validateInputData(filePath) {
-  function hasProp(object, key) {
-    return Object.prototype.hasOwnProperty.call(object, key);
-  }
-
-  let orders = [];
-  let response = { msg: '', err: false };
-  try {
-    orders = JSON.parse(fs.readFileSync(filePath));
-    // console.log(orders);
-  } catch (error) {
-    response = { msg: 'File is not valid JSON', err: true };
-  }
-  if (orders.length === 0) {
-    response = { msg: 'Data set has no records', err: true };
-  }
-  orders.forEach((order, i) => {
-    // || !order.brew_time || !order.type
-    if (!hasProp(order, 'order_id')) {
-      response = { msg: `Order ${i} is missing "order_id" prop`, err: true };
-    } else if (!hasProp(order, 'order_time')) {
-      response = { msg: `Order ${i} is missing "order_time" prop`, err: true };
-    } else if (!hasProp(order, 'type')) {
-      response = { msg: `Order ${i} is missing "type" prop`, err: true };
+export default class sortFifo {
+  constructor(props) {
+    // super(props);
+    this.state = {
+      baristaOneFreeAt: 0,
+      baristaTwoFreeAt: 0,
+      startingTime: 0,
     }
-  });
-  return response;
+  }
+
+  assignBaristaToOrder({ order_time, order_id, brewTime }) {
+    const {
+      baristaOneFreeAt,
+      startingTime,
+      baristaTwoFreeAt
+    } = this.state;
+
+    if (baristaOneFreeAt <= order_time) { // barista 1 is free
+      if (order_time === startingTime) {// barista 1 is starting the shift
+        this.state.baristaOneFreeAt = startingTime + brewTime;
+      } else { // barista 1 is idle and waiting new order
+        this.state.baristaOneFreeAt = order_time + brewTime;
+      }
+
+      return {
+        order_id,
+        barista_id: 1,
+        start_time: order_time // barista will start right away
+      }
+
+    } else if (baristaTwoFreeAt <= order_time) { // barista 2 is free
+
+      if (order_time === startingTime) {
+        // barista 2 is starting the shift
+        this.state.baristaTwoFreeAt = startingTime + brewTime;
+      } else {
+        // barista 2 is idle and waiting new order
+        this.state.baristaTwoFreeAt = order_time + brewTime;
+      }
+
+      return {
+        order_id,
+        barista_id: 2,
+        start_time: order_time, // barista will start right away
+      }
+    }
+
+    // if both are busy, call the one is finishing sooner
+    if (baristaOneFreeAt < baristaTwoFreeAt) {
+      // barista one is free first
+      this.state.baristaOneFreeAt = baristaOneFreeAt + brewTime;
+      return {
+        order_id,
+        barista_id: 1,
+        start_time: baristaOneFreeAt //if barista is free, start time === order time
+      }
+    } else {
+      // barista two is free first
+      this.state.baristaTwoFreeAt = baristaTwoFreeAt + brewTime;
+      return {
+        order_id,
+        barista_id: 2,
+        start_time: baristaTwoFreeAt //if barista is free, start time === order time
+      }
+    }
+  }
+
+  createNewOrderID(orders) {
+    const orderIDs = orders.map(drink => {
+      return drink.order_id;
+    });
+    orderIDs.sort((a, b) => a - b);
+    return orderIDs[orderIDs.length - 1] + 1;
+  }
+
+  getLastOrderTime(orders) {
+    const ordersTimes = orders.map(drink => {
+      return drink.order_time;
+    });
+    ordersTimes.sort((a, b) => a - b);
+    return ordersTimes[ordersTimes.length - 1];
+  }
+
+  setStartingTime(orders) {
+    const orderTimes = orders.map(drink => {
+      return drink.order_time;
+    });
+    orderTimes.sort((a, b) => a - b);
+    this.state.startingTime = orderTimes[0];
+  }
+
+  sortOrders(orders) {
+    this.setStartingTime(orders);
+    const drinks = orders.map((order, i) => {
+      const brewTime = Menu.find(item => item.type === order.type).brew_time;
+      const newDrink = this.assignBaristaToOrder({
+        ...order,
+        brewTime,
+      });
+      return newDrink;
+    });
+    return drinks;
+  }
 }
