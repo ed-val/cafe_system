b5154285b07b5152948ef11957a7347d683b424c
ed-val2 <e.valbuena.d@gmail.com>
2020-05-05 10:50:49 -0500

    corrected format

--- src/methods/menu.js
+++ src/methods/menu.js
@@ -1,5 +1,5 @@
 export default [
-	{ type: 'tea', brew_time: 3, profit: 2 },
+	{ type: "tea", brew_time: 3, profit: 2 },
 	{ type: "latte",    brew_time: 4, profit: 3 },
 	{ type: "affogato", brew_time: 7, profit: 5 }
 ]
