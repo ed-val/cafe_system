67e25bd9ec50e584350c49f55b8b99b469223f51
ed-val2 <e.valbuena.d@gmail.com>
2020-05-05 00:28:13 -0500

    added new tasks and validations

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import chalk from 'chalk';
 import fs from 'fs';
+import Listr from 'listr';
+import Fifo from './methods/fifo';
+import Optimized from './methods/optimized';
+const boxen = require('boxen');
+let orders = [];
+let drinks = []; // processed orders
+let inputOK = true;
 // import ncp from 'ncp';
 // import path from 'path';
 // import { promisify } from 'util';
 // import execa from 'execa';
-import Listr from 'listr';
-import { validateInputData } from './methods/fifo';
 // import { projectInstall } from 'pkg-install';
 
 // const access = promisify(fs.access);
@@ -26,9 +31,40 @@ import { validateInputData } from './methods/fifo';
 //   }
 //   return;
 // }
+function skipFileValidation(newOrders) {
+  return newOrders.length > 0
+    ? 'Input is no longer a JSON file'
+    : undefined;
+}
+
+export function validateInputData(filePath) {
+  function hasProp(object, key) {
+    return Object.prototype.hasOwnProperty.call(object, key);
+  }
+
+  let response = { msg: '', err: false };
+  try {
+    orders = JSON.parse(fs.readFileSync(filePath));
+    // console.log(orders);
+  } catch (error) {
+    response = { msg: 'File is not valid JSON', err: true };
+  }
+  if (orders.length === 0) {
+    response = { msg: 'Data set has no records', err: true };
+  }
+  orders.forEach((order, i) => {
+    if (!hasProp(order, 'order_id')) {
+      response = { msg: `Order ${i} is missing "order_id" prop`, err: true };
+    } else if (!hasProp(order, 'order_time')) {
+      response = { msg: `Order ${i} is missing "order_time" prop`, err: true };
+    } else if (!hasProp(order, 'type')) {
+      response = { msg: `Order ${i} is missing "type" prop`, err: true };
+    }
+  });
+  return response;
+}
 
 export async function validateFilePath(filePath) {
-  let data = {};
   try {
     await fs.promises.access(filePath);
     // The check succeeded
@@ -51,52 +87,89 @@ export function validateFileType(filePath) {
   return true;
 }
 
-export async function startCafe(options) {
-  // options = {
-  //   ...options,
-  //   targetDirectory: options.targetDirectory || process.cwd(),
-  // };
+function validateOrdersTimes(orders) {
+  let response = { msg: '', err: false };
+  orders.forEach(({ order_time }, i) => {
+    if (order_time > 100) {
+      response = {
+        msg: `Order ${i} was made after working hours (${order_time})`,
+        err: true,
+      }
+    }
+  });
+  return response;
+}
 
-  // const currentFileUrl = import.meta.url;
-  // const templateDir = path.resolve(
-  //   new URL(currentFileUrl).pathname,
-  //   '../../templates',
-  //   options.template.toLowerCase()
-  // );
-  // options.templateDirectory = templateDir;
+function logInputData(data, boxColor, header) {
+  const dataForUser = data.map(item => JSON.stringify(item) + "\n");
+  console.log(chalk.cyan.bold(header));
+  console.log(
+    boxen(
+      dataForUser.toString(),
+      { padding: 1, borderColor: boxColor, borderStyle: 'double' }
+    )
+  );
+}
 
-  // try {
-  //   await access(templateDir, fs.constants.R_OK);
-  // } catch (err) {
-  //   console.error('%s Invalid template name', chalk.red.bold('ERROR'));
-  //   process.exit(1);
-  // }
-
-  const tasks = new Listr([
+export async function startCafe(options, newOrders) {
+  const fifo = new Fifo();
+  const fifoTasks = new Listr([
     {
-      title: 'Validating file path',
-      task: async () => {
-        if(!await validateFilePath(options.jsonPath)) {
-          throw new Error('Input file does not exist');
+      title: 'Validating order times',
+      task: () => {
+        const evaluation = validateOrdersTimes(
+          newOrders.length > 0 ? newOrders : orders
+        );
+        if (evaluation.err) {
+          throw new Error(evaluation.msg);
         }
       }
     },
     {
+      title: 'Working fifo algorithm',
+      task: () => {
+        drinks = fifo.sortOrders(newOrders.length > 0 ? newOrders : orders);
+      }
+    },
+  ]);
+
+  const optimizedTasks = new Listr([
+    {
+      title: 'Working optimized algorithm',
+      task: () => {
+        const optimized = new Optimized();
+      }
+    },
+  ]);
+
+  const validationTasks = new Listr([
+    {
+      title: 'Validating file path',
+      task: async () => {
+        if (!await validateFilePath(options.jsonPath)) {
+          throw new Error('Input file does not exist at this path');
+        }
+      },
+      skip: () => skipFileValidation(newOrders),
+    },
+    {
       title: 'Validating file extension',
       task: () => {
-        if(!validateFileType(options.jsonPath)) {
+        if (!validateFileType(options.jsonPath)) {
           throw new Error('Input file is not valid JSON data');
         }
-      }
+      },
+      skip: () => skipFileValidation(newOrders),
     },
     {
       title: 'Validating input data',
       task: () => {
         const response = validateInputData(options.jsonPath);
-        if(response.err) {
+        if (response.err) {
           throw new Error(response.msg);
         }
-      }
+      },
+      skip: () => skipFileValidation(newOrders),
     },
     // {
     //   title: 'Sorting Baristas Orders',
@@ -115,10 +188,40 @@ export async function startCafe(options) {
     // },
   ]);
 
-  await tasks.run();
-  // console.log('Copy project files');
-  // await copyTemplateFiles(options);
+  try {
+    console.log(chalk.cyan.bold('VALIDATING INPUT FILE'));
+    await validationTasks.run();
+    console.log('%s Input ready', chalk.green.bold('DONE'));
+    inputOK = true;
+  } catch (error) {
+    console.log('%s Input not ready', chalk.red.bold('ERROR'));
+    inputOK = false;
+  }
 
-  console.log('%s Input ready', chalk.green.bold('DONE'));
-  return true;
+  if (options.method === 'fifo' && inputOK) {
+    try {
+      console.log(chalk.cyan.bold('BEGGINING FIFO SORTING'));
+      await fifoTasks.run();
+      if (options.logInput) logInputData(
+        newOrders.length > 0 ? newOrders : orders,
+        'blueBright',
+        'Input',
+      );
+      logInputData(drinks, 'yellow', 'Output');
+      console.log('%s Tasks ran ok', chalk.green.bold('DONE'));
+    } catch (error) {
+      console.log('%s One or more issues with tasks', chalk.red.bold('ERROR'));
+      // console.error(error);
+    }
+  }
+
+  if (options.method === 'optimized' && inputOK) {
+    console.log(chalk.cyan.bold('BEGGINING OPTIMIZED SORTING'));
+    await optimizedTasks.run();
+    console.log('%s Tasks ran ok', chalk.green.bold('DONE'));
+  }
+  return {
+    drinks,
+    orders: newOrders.length > 0 ? newOrders : orders
+  };
 }
