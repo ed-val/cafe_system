748f0adcc2af1eaa505936c97f72153f7f29c060
ed-val2 <e.valbuena.d@gmail.com>
2020-05-05 09:54:34 -0500

    refactored sorting algo so its more readable

--- src/methods/fifo.js
+++ src/methods/fifo.js
@@ -2,7 +2,6 @@ import Menu from './menu';
 
 export default class sortFifo {
   constructor(props) {
-    // super(props);
     this.state = {
       baristaOneFreeAt: 0,
       baristaTwoFreeAt: 0,
@@ -10,60 +9,75 @@ export default class sortFifo {
     }
   }
 
-  assignBaristaToOrder({ order_time, order_id, brewTime }) {
-    const {
-      baristaOneFreeAt,
-      startingTime,
-      baristaTwoFreeAt
-    } = this.state;
+  _baristaOneStartsDrink({order_time, brewTime}) {
+    const { startingTime } = this.state;
 
-    if (baristaOneFreeAt <= order_time) { // barista 1 is free
-      if (order_time === startingTime) {// barista 1 is starting the shift
-        this.state.baristaOneFreeAt = startingTime + brewTime;
-      } else { // barista 1 is idle and waiting new order
-        this.state.baristaOneFreeAt = order_time + brewTime;
-      }
-
-      return {
-        order_id,
-        barista_id: 1,
-        start_time: order_time // barista will start right away
-      }
-
-    } else if (baristaTwoFreeAt <= order_time) { // barista 2 is free
-
-      if (order_time === startingTime) {
-        // barista 2 is starting the shift
-        this.state.baristaTwoFreeAt = startingTime + brewTime;
-      } else {
-        // barista 2 is idle and waiting new order
-        this.state.baristaTwoFreeAt = order_time + brewTime;
-      }
-
-      return {
-        order_id,
-        barista_id: 2,
-        start_time: order_time, // barista will start right away
-      }
+    if (order_time === startingTime) {// barista 1 is starting the shift
+      this.state.baristaOneFreeAt = startingTime + brewTime;
+    } else { // barista 1 is idle and waiting new order
+      this.state.baristaOneFreeAt = order_time + brewTime;
     }
 
-    // if both are busy, call the one is finishing sooner
-    if (baristaOneFreeAt < baristaTwoFreeAt) {
-      // barista one is free first
-      this.state.baristaOneFreeAt = baristaOneFreeAt + brewTime;
+    return {
+      order_id,
+      barista_id: 1,
+      start_time: order_time // barista will start right away
+    }
+  }
+
+  _baristaTwoStartsDrink({order_time, brewTime}) {
+    const { startingTime } = this.state;
+
+    if (order_time === startingTime) { // barista 2 is starting the shift
+      this.state.baristaTwoFreeAt = startingTime + brewTime;
+    } else { // barista 2 is idle and waiting new order
+      this.state.baristaTwoFreeAt = order_time + brewTime;
+    }
+
+    return {
+      order_id,
+      barista_id: 2,
+      start_time: order_time, // barista will start right away
+    }
+  }
+
+  _baristaOneWillStartDrink(brewTime) {
+    const { baristaOneFreeAt } = this.state;
+    this.state.baristaOneFreeAt = baristaOneFreeAt + brewTime;
       return {
         order_id,
         barista_id: 1,
         start_time: baristaOneFreeAt //if barista is free, start time === order time
       }
+  }
+
+  _baristaTwoWillStartDrink(brewTime) {
+    const { baristaTwoFreeAt } = this.state;
+    this.state.baristaTwoFreeAt = baristaTwoFreeAt + brewTime;
+    return {
+      order_id,
+      barista_id: 2,
+      start_time: baristaTwoFreeAt //if barista is free, start time === order time
+    }
+  }
+
+  assignOrder(orderDetails) {
+    const { baristaOneFreeAt, baristaTwoFreeAt } = this.state;
+
+    if (baristaOneFreeAt <= orderDetails.order_time) { // barista 1 is free
+      return this._baristaOneStartsDrink(orderDetails);
+
+    } else if (baristaTwoFreeAt <= orderDetails.order_time) { // barista 2 is free
+      return this._baristaTwoStartsDrink(orderDetails);
+    }
+
+    // if both are busy, call the one is finishing sooner
+    if (baristaOneFreeAt < baristaTwoFreeAt) {
+      // barista one will be ready first
+      return this._baristaOneWillStartDrink(orderDetails.brewTime);
     } else {
-      // barista two is free first
-      this.state.baristaTwoFreeAt = baristaTwoFreeAt + brewTime;
-      return {
-        order_id,
-        barista_id: 2,
-        start_time: baristaTwoFreeAt //if barista is free, start time === order time
-      }
+      // barista two will be ready first
+      return this._baristaTwoWillStartDrink(orderDetails.brewTime);
     }
   }
 
@@ -95,7 +109,7 @@ export default class sortFifo {
     this.setStartingTime(orders);
     const drinks = orders.map((order, i) => {
       const brewTime = Menu.find(item => item.type === order.type).brew_time;
-      const newDrink = this.assignBaristaToOrder({
+      const newDrink = this.assignOrder({
         ...order,
         brewTime,
       });
