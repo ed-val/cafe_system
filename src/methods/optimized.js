01a45e1b1374f2321350201b86888f8b18edc84d
ed-val2 <e.valbuena.d@gmail.com>
2020-05-05 00:28:49 -0500

    started class

---
+++ src/methods/optimized.js
@@ -0,0 +1,17 @@
+import Menu from './menu';
+
+export default class sortOptimized {
+  constructor(props) {
+    // super(props);
+    this.state = {
+      baristaOneFreeAt: 0,
+      baristaTwoFreeAt: 0,
+      time: 0,
+    }
+    console.log('OPTIMIZED', Menu);
+  }
+
+  sortOrders(oders) {
+    console.log(orders);
+  }
+}
